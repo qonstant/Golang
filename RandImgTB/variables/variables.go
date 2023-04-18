15e21c00758bcde342bf385a6b2d937c1c82d93a
Rakymzhan Zhabagin <rakorako2204@gmail.com>
2023-04-18 23:49:44 +0600

    tb_bot_v1

---
+++ RandImgTB/variables/variables.go
@@ -0,0 +1,10 @@
+package variables
+
+const (
+	UnsplashAPI = "https://api.unsplash.com/photos/random?client_id="
+)
+
+var (
+	UnsplashToken = "xnhdL03-VeQYXtRrE06gbxWUMGPVwVOmDTwryg9ZOuY"
+	BotToken = "6100591907:AAH_lr9SrUT2NZvSkevQ1xbHpMxb8UkOBzo"
+)
